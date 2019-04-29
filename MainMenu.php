eea4692298ce1bbed9873160318d7e7e6b046122
Sandip Hodkhasa <34456224+shodkhasa@users.noreply.github.com>
2019-04-29 07:45:12 -0700

    Update MainMenu.php

--- MainMenu.php
+++ MainMenu.php
@@ -3,25 +3,24 @@
 	session_start();
 	$name_email = 'name=' . $_SESSION['name'] . 
 				  '&email=' . $_SESSION['email'];
-	#print $name_email;
 ?>
 <ul class="ul_css">
 <li class="li_css">
-	<a class="li_css_a" href = "main.php?<?php print $name_email; ?>">Main</a>
+	<a class="li_css_a" href = "main.php?<?php print $name_email;?>">Main</a>
 </li>
 <li class="li_css">
-	<a class="li_css_a" href = "shopping.php?<?php print $name_email; ?>">Shopping</a>
+	<a class="li_css_a" href = "shopping.php?<?php print $name_email;?>">Shopping</a>
 </li>
 <li class="li_css">
-	<a class="li_css_a" href = "selling.php?<?php print $name_email; ?>">Selling</a>
+	<a class="li_css_a" href = "selling.php?<?php print $name_email;?>">Selling</a>
 </li>
 <li class="li_css">
-	<a class="li_css_a" href = "profile.php?<?php print $name_email; ?>">Profile</a>
+	<a class="li_css_a" href = "profile.php?<?php print $name_email;?>">Profile</a>
 </li>
 <li class="li_css">
-	<a class="li_css_a" href = "forums.php?<?php print $name_email; ?>">Forums</a>
+	<a class="li_css_a" href = "forums.php?<?php print $name_email;?>">Forums</a>
 </li>
 <li class="li_css" style="float:right">
-	<a class="li_css_a" href = "logout.php?<?php print $name_email; ?>">Logout</a>
+	<a class="li_css_a" href = "logout.php?<?php print $name_email;?>">Logout</a>
 </li>
 </ul>
