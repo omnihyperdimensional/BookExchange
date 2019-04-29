acf567223ca4e0375a8367f2abcf8137f770f5b5
Sandip Hodkhasa <34456224+shodkhasa@users.noreply.github.com>
2019-04-29 07:45:24 -0700

    Update shopping.php

--- shopping.php
+++ shopping.php
@@ -1,9 +1,18 @@
 <!doctype html>
 <html>
 <head>
-<meta charset="utf-8">
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<title>Shopping Page</title>
+  <!-- Meta Tags -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <!-- Styles -->
+  <style>
+
+  </style>
+  <title>Shopping Page</title>
 </head>
 
 <body>
@@ -14,64 +23,29 @@
 	include("Connect_Database.php");
 ?>
 <?php
-	$selectBooks = "select * from books;";
+	$selectBooks = "SELECT * FROM books b JOIN users u ON b.seller_id = u.id;";
 	$results = mysqli_query($connect, $selectBooks);
 ?>
-<nav>
-</nav>
-	<table align="center" border="2" width=400>
+<table align="center" border="2" width=400>
 	<tr>
-		<th>
-			Name
-		</th>
-		<th>
-			Email
-		</th>
-		<th>
-			Title
-		</th>
-		<th>
-			Post Time
-		</th>
-		<th>
-			Picture
-		</th>
+		<th>Seller</th>
+		<th>Title</th>
+		<th>Price</th>
+		<th>Date Posted</th>
+		<th>Picture</th>
 	</tr>
 <?php
-while($row = mysqli_fetch_assoc($results))
-{
+while($row = mysqli_fetch_assoc($results)) {
 	print "<tr>";
-	print "<td>";
-	print ($row["name"]);
-	print "</td>";
-	print "<td>";
-	print ($row["email"]);
-	print "</td>";
-	print "<td>";
-	print ($row["title"]);
-	print "</td>";
-	print "<td>";
-	print ($row["posttime"]);
-	print "</td>";
-	print "<td>";
-	print "<img src ='";
-	print $row["picpath"] . "' height = 50 width = 50>";
-	print "</td>";
-	print "<td align = 'center'; >";
-    print "<a href=".$row["tocpicpath"]." target=_blank><i class=material-icons>picture_as_pdf</i></a>";
-    print "</td>";
-	
-	/*Print "<td>";
-	Print "<a href='UserDelete.php?";
-	Print "email=" . $row["email"] . "'>";
-	Print "DELETE";
-	Print "</a>";
-	Print "</td>";
-	*/
+	print "<td>" . ($row["name"]) . "</td>";
+	print "<td>" . "<a href='BookInformation.php?isbn10=" . ($row["isbn10"]) . "'>";
+	print ($row["title"]) . "</a>" . "</td>";
+	print "<td>" . ($row["price"]) . "</td>";
+	print "<td>" . substr($row["post_time"], 0, 10) . "</td>";
+	print "<td>" . "<img src='" . $row["pic_path"] . "' height = 190 width = 150>" . "</td>";
 	print "</tr>";
 }
 ?>
-
 </table>
 </body>
 </html>
