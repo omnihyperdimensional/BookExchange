4242765bf7c8d8f4000854d2e88a596af98c2029
Sandip Hodkhasa <34456224+shodkhasa@users.noreply.github.com>
2019-04-29 07:44:06 -0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,62 +1,40 @@
 <!doctype html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<link href="MainMenu.css" type="text/css" rel="stylesheet"/>
-		<title>Administrator Page</title>
-	</head>
+<head>
+<meta charset="UTF-8">
+<title>Administrator Page</title>
+</head>
 
-	<body>	
-		<?php
-			include("Connect_Database.php");
-		?>
-		<?php
-			$selectUsers = "select * from users;";
-			$results = mysqli_query($connect, $selectUsers);
-		?>
-		<nav>
-			</nav>
-		<?php
-	    	session_start();
-	    	$name_email = 'name=' . $_SESSION['name'] . '&email=' . $_SESSION['email'];
-	    	#print $name_email;
-		?>
-        <ul class="ul_css">
-			<a class="li_css_a" style="float:left" href = "UserEnroll.php">Go to Enrollment Page</a>
-			<a class="li_css_a" style="float:right" href = "logout.php?<?php print $name_email; ?>">Logout</a>
-            <a class="li_css_a" style="float:right" href = "mail.php?<?php print $name_email; ?>">Email</a>
-        </ul>
-		<table align="center" border="2" width=400>
-			<tr>
-				<th>
-					Name
-				</th>
-				<th>
-					Email
-				</th>
-				<th>
-					Delete
-				</th>
-			</tr>
-		<?php
-			while($row = mysqli_fetch_assoc($results))
-			{
-				print "<tr>";
-				print "<td>";
-				print ($row["name"]);
-				print "</td>";
-				print "<td>";
-				print ($row["email"]);
-				print "</td>";
-				Print "<td>";
-				Print "<a href='UserDelete.php?";
-				Print "email=" . $row["email"] . "'>";
-				Print "DELETE";
-				Print "</a>";
-				Print "</td>";
-				print "</tr>";
-			}
-		?>
-	</table>
+<body>
+<?php
+	include("Connect_Database.php");
+?>
+<?php
+	include("MainMenu.php");
+?>
+<?php
+	$selectUsers = "SELECT * FROM users;";
+	$results = mysqli_query($connect, $selectUsers);
+?>
+<nav>
+	<a href = "UserEnroll.php">Go to Enrollment Page</a>
+	<a style="float:right" href = "logout.php">Logout</a>
+</nav>
+<table align="center" border="2" width=400>
+	<tr>
+		<th>Name</th>
+		<th>Email</th>
+		<th>Delete</th>
+	</tr>
+<?php
+	while($row = mysqli_fetch_assoc($results)) {
+		print "<tr>";
+		print "<td>" . ($row["name"]) . "</td>";
+		print "<td>" . ($row["email"]). "</td>";
+		print "<td><a href='UserDelete.php?email=" . $row["email"] . "'>DELETE</a></td>";
+		print "</tr>";
+	}
+?>
+</table>
 </body>
 </html>
