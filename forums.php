0cc14e85e6c4f9581b669bb4a91a602789877355
Sandip Hodkhasa <34456224+shodkhasa@users.noreply.github.com>
2019-04-29 07:44:32 -0700

    Update forums.php

--- forums.php
+++ forums.php
@@ -1,7 +1,6 @@
 <!doctype html>
 <html>
 <head>
-<link href="MainMenu.css" type="text/css" rel="stylesheet"/>
 <meta charset="utf-8">
 <title>Forums</title>
 </head>
@@ -17,86 +16,78 @@
 	$selectForums = "select * from forum;";
 	$results = mysqli_query($connect, $selectForums);
 ?>
-<table width = 100%>
+<nav>
+</nav>
+	<table align="center" border="2" width=400>
 	<tr>
-		<td>
-			<table width=100%>
-				<tr>
-					<th>
-						Name
-					</th>
-					<th>
-						Email
-					</th>
-					<th>
-						Post
-					</th>
-				</tr>
-				<tr>
-					<?php
-						while($row = mysqli_fetch_assoc($results))
-						{
-							print "<tr>";
-							print "<td>";
-							print ($row["name"]);
-							print "</td>";
-							print "<td>";
-							print ($row["email"]);
-							print "</td>";
-							print "<td>";
-							print ($row["post"]);
-							print "</td>";
-					
-							
-									/*Print "<td>";
-									Print "<a href='UserDelete.php?";
-									Print "email=" . $row["email"] . "'>";
-									Print "DELETE";
-									Print "</a>";
-									Print "</td>";
-									*/
-							print "</tr>";
-						}
-					?>
-				</tr>			
-			</table>
-		</td>
-		<td>
-			<form action= "forumsinsert.php" method = "post">
-				<table align="center">
-					<tr>
-						<td>
-							User Name
-						</td>
-						<td>
-							<input type="text" name="name" value="<?php print $_GET['name']?>"/>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							Email
-						</td>
-						<td>
-							<input type="text" name= "email" value="<?php print $_GET['email']?>"/>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							Post
-						</td>
-						<td>
-							<input type="text" name= "post" />
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<input type="submit" value="submit"/>
-						</td>
-					</tr>
-				</table>
-			</form>
-		</td>
+		<th>
+			Name
+		</th>
+		<th>
+			User ID
+		</th>
+		<th>
+			Post
+		</th>
 	</tr>
+<?php
+while($row = mysqli_fetch_assoc($results))
+{
+	print "<tr>";
+	print "<td>";
+	print ($row["poster_name"]);
+	print "</td>";
+	print "<td>";
+	print ($row["poster_id"]);
+	print "</td>";
+	print "<td>";
+	print ($row["post"]);
+	print "</td>";
+	
+	
+	/*Print "<td>";
+	Print "<a href='UserDelete.php?";
+	Print "email=" . $row["email"] . "'>";
+	Print "DELETE";
+	Print "</a>";
+	Print "</td>";
+	*/
+	print "</tr>";
+}
+?>
+
 </table>
+<form action= "forumsinsert.php" method = "post">
+<table align="center">
+<tr>
+<td>
+User Name
+</td>
+<td>
+<input type="text" name="name" value="<?php print $_SESSION['name']?>"/>
+</td>
+</tr>
+<tr>
+<td>
+Id
+</td>
+<td>
+<input type="text" name= "id" value="<?php print $_SESSION['id']?>"/>
+</td>
+</tr>
+<tr>
+<td>
+Post
+</td>
+<td>
+<input type="text" name= "post" />
+</td>
+</tr>
+<tr>
+<td>
+<input type="submit" value="submit"/>
+</td>
+</tr>
+</form>
 </body>
 </html>
