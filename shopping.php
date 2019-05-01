f0bd455b6241c7f17edcb0bd92e77e8cbf6e1665
Guadalupe Sahagun <lupecsahagun23@yahoo.com>
2019-05-01 14:39:59 -0700

    Update shopping.php

--- shopping.php
+++ shopping.php
@@ -1,53 +1,64 @@
 <!doctype html>
 <html>
+
 <head>
-  <!-- Meta Tags -->
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Meta Tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-  <!-- Styles -->
-  <style>
+    <!-- Styles -->
+    <style>
+    body {
+        margin: 0;
+    }
+    </style>
 
-  </style>
-  <title>Shopping Page</title>
+    <title>Shopping</title>
 </head>
 
 <body>
-<?php
+    <?php
 	include("MainMenu.php");
 ?>
-<?php
+    <?php
 	include("Connect_Database.php");
 ?>
-<?php
+    <?php
 	$selectBooks = "SELECT * FROM books b JOIN users u ON b.seller_id = u.id;";
 	$results = mysqli_query($connect, $selectBooks);
 ?>
-<table align="center" border="2" width=400>
-	<tr>
-		<th>Seller</th>
-		<th>Title</th>
-		<th>Price</th>
-		<th>Date Posted</th>
-		<th>Picture</th>
-		<th>Table of Content</th>
-	</tr>
-<?php
+    <h2 align="center" style="margin-top:5px; margin-bottom:5px;">SHOP</h2>
+    <div class="fluid-container">
+        <div class="col">
+            <table align="center" border="2" width=400 class="table table-bordered table-sm">
+                <thead class="thead-light">
+                    <tr>
+                        <th>Seller</th>
+                        <th>Title</th>
+                        <th>Price</th>
+                        <th>Date Posted</th>
+                        <th>Picture</th>
+                    </tr>
+                </thead>
+                <?php
 while($row = mysqli_fetch_assoc($results)) {
 	print "<tr>";
 	print "<td>" . ($row["name"]) . "</td>";
 	print "<td>" . "<a href='BookInformation.php?isbn10=" . ($row["isbn10"]) . "'>";
 	print ($row["title"]) . "</a>" . "</td>";
-	print "<td>$" . ($row["price"]) . "</td>";
+	print "<td>" . ($row["price"]) . "</td>";
 	print "<td>" . substr($row["post_time"], 0, 10) . "</td>";
 	print "<td>" . "<img src='" . $row["pic_path"] . "' height = 190 width = 150>" . "</td>";
-	print "<td>" . "<img src='" . $row["toc_path"] . "' height = 190 width = 150>" . "</td>";
 	print "</tr>";
 }
 ?>
-</table>
+            </table>
+        </div>
+    </div>
 </body>
+
 </html>
