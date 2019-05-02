5b3a947954295dbaca9ec7fcdb94dbad02a7147e
Guadalupe Sahagun <lupecsahagun23@yahoo.com>
2019-05-02 15:52:24 -0700

    Cleanup
    Resized some things so that it would all fit within the page.

--- forums.php
+++ forums.php
@@ -2,8 +2,14 @@
 <html>
 
 <head>
-<meta charset="utf-8">
-<title>Forum</title>
+    <meta charset="utf-8">
+    <title>Forum</title>
+    <!--style-->
+    <style>
+    * {
+        margin: 0;
+    }
+    </style>
 </head>
 
 <body>
@@ -17,20 +23,21 @@
 	$selectForums = "SELECT * FROM forum;";
 	$results = mysqli_query($connect, $selectForums);
     ?>
-    <nav>
-    </nav>
+
     <h2 align="center" style="margin-top:20px; margin-bottom:20px;">FORUM</h2>
-    <div class="fluid-container">
+
+    <div class="container">
         <div class="row">
-            <div class="col-8">
-                <table class="table table-bordered table-sm" style="margin-left: 50px;">
+            <div class="col-10">
+                <table class="table table-bordered">
                     <thead class="thead-light">
                         <tr>
                             <th>NAME</th>
                             <th>POST</th>
                         </tr>
                         <?php
-                        while($row = mysqli_fetch_assoc($results)){
+                        while($row = mysqli_fetch_assoc($results))
+                        {
                             print "<tr>";
                             print "<td>";
                             print ($row["poster_name"]);
@@ -45,24 +52,25 @@
                 </table>
             </div>
 
-            <div class="col-4">
+            <div class="col-2">
                 <form action="forumsinsert.php" method="post">
-                    <table align="center" style="margin-left: 50px;">
-                        <tr>
-                            <td>Posting as: &nbsp;&nbsp;&nbsp; <?php print $_SESSION['name']?></td>
-                            <td style="text-align: left;"><input type="hidden" name="name" value="<?php print $_SESSION['name']?>"/></td>
-                            <input type="hidden" name="id" value="<?php print $_SESSION['id']?>" />
-                        </tr>
-                        <tr>
-                            <td colspan="2"><textarea rows="6" cols="40" name="post" ></textarea></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td style="text-align: right;"><input class="btn btn-primary" style="width: 100px; height: 40px;" type="submit" value="Post"></td>
-                        </tr>
+                    <tr>
+                        <td>POSTING AS: <input type="hidden" name="name" value="<?php print $_SESSION['name']?>" />
+                            <?php print $_SESSION['name']?></td>
+                        <input type="hidden" name="id" value="<?php print $_SESSION['id']?>" />
+                    </tr>
+                    <tr>
+                        <td><textarea rows="6" cols="30" name="post"></textarea></td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit" value="POST"
+                                style="width: 100px; background: #4a4f55; color: #ffffff;border:1px solid #ffffff;" />
+                        </td>
+                    </tr>
                 </form>
             </div>
         </div>
     </div>
 </body>
+
 </html>
