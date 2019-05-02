25ec12516851a90a87d15edc8c4fc632377d7691
Kevin J Crespin <kevinjonathan290@gmail.com>
2019-05-02 15:16:15 -0700

    SELLING LAS VER
    FRONT END AND BACK END DONE

--- selling.php
+++ selling.php
@@ -3,59 +3,69 @@
 <head>
 <meta charset = "utf-8">
 <title>Selling Page</title>
+<style>
+	th {
+		background-color: #4a4f55;
+		color: #ffffff;
+		font-weight:normal;
+	}
+</style>
 </head>
 
 <body>
 <?php
 	include("MainMenu.php");
 ?>
-<form action= "sellingInsert.php" enctype="multipart/form-data" method = "post">
-<table align="center">
-<tr>
-<td>
-User Name
-</td>
-<td>
-<input type="text" name="name" value="<?php print $_GET['name']?>"/>
-</td>
-</tr>
-<tr>
-<td>
-Email
-</td>
-<td>
-<input type="text" name= "email" value="<?php print $_GET['email']?>"/>
-</td>
-</tr>
-<tr>
-<td>
-Title
-</td>
-<td>
-<input type="text" name= "title" />
-</td>
-</tr>
-<tr>
-<td>
-Description
-</td>
-<td>
-<input type="text" name= "description" />
-</td>
-</tr>
-<tr>
-<td>
-Book Picture
-</td>
-<td>
-<input type="file" name= "pic" />
-</td>
-</tr>
-<tr>
-<td>
-<input type="submit" value="submit"/>
-</td>
-</tr>
-</form>
+<h2 align="center" style="margin-top:20px; margin-bottom:20px;">SELL</h2>
+
+<div class="container">
+	<div class="row justify-content-md-center">
+    	<div class="col col-lg-2">
+    	</div>
+    	<div class="col-md-auto">
+			<form action="sellingInsert.php" enctype="multipart/form-data" method ="post">
+			<table class="table table-bordered table-sm">
+				<tr>
+					<th>SELLING AS</th>
+					<td width="50"><input type="hidden" name="name" value="<?php print $_GET['name']?>"/>&nbsp;&nbsp;&nbsp;<?php print $_GET['name']?></td>
+				</tr>
+				<tr>
+					<th>TITLE</th>
+					<td width="50">
+						<input type="text" name="title" maxlength="100" size="50" required/>
+						<input type="hidden" name="email" value="<?php print $_GET['email']?>"/>
+					</td>
+				</tr>
+				<tr>
+					<th>ISBN-10</th>
+					<td width="50"><input type="text" name="isbn10" maxlength="10" size="50" required/></td>
+				</tr>
+				<tr>
+					<th>AUTHOR</th>
+					<td width="50"><input type="text" name="author" maxlength="50" size="50" required/></td>
+				</tr>
+				<tr>
+					<th>PRICE</th>
+					<td width="50">$ <input type="number" min="0.01" step="0.01" max="2500" name="price" maxlength="6" size="5" required/></td>
+				</tr>
+				<tr>
+					<th>DESCRIPTION</th>
+					<td width="50"><textarea rows="6" cols="52" name="description" maxlength="500" required></textarea></td>
+				</tr>
+				<tr>
+					<th>PICTURE</th>
+					<td width="50"><input type="file" name="pic" required/></td>
+				</tr>
+				<tr style="border: none">
+					<td style="border: none"></td>
+					<td style="text-align: right; border: none" width="50"><input class="btn btn-primary" style="width: 100px; height: 40px;" type="submit" value="Sell"></td>
+				</tr>
+			</table>
+			</form>
+    	</div>
+    	<div class="col col-lg-2">
+    	</div>
+  	</div>
+</div>
 </body>
 </html>
