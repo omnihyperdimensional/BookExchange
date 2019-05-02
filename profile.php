72ac8e3293103bab1e0091e9b8979de1fd0ec028
Guadalupe Sahagun <lupecsahagun23@yahoo.com>
2019-05-01 21:36:56 -0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -2,15 +2,28 @@
 <html>
 
 <head>
-	<meta charset="utf-8">
-	<title>Profile Page</title>
+    <meta charset="utf-8">
+    <title>Profile Page</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        h3 {
+            background-color: #FFCE00;
+            border: 1px solid #FFCE00;
+            margin: 25px 0 25px 0;
+            text-align: center;
+        }
+        a{
+            color: white;
+        }
+    </style>
 </head>
 
 <body>
-	<?php
+    <?php
 	include("MainMenu.php");
 	?>
-	<?php
+    <?php
 	include("Connect_Database.php");
 	?>
 	
@@ -86,45 +99,44 @@
 
 
 
-	<h1>Welcome <?php print $_SESSION['name']?></h1>
-	<div>
-		<h3>Your account balance:</h3>
-		Your Current Balance is:<br>
-		<table align="center" border="1" width=400>
-			<tr>
-				<th>Balance</th>
-			</tr>
-			<tr>
-				<td>
-					<?php
-					if($lowBalance!=null){
-						print "<h6><font color=red><strong>$" . ($data["balance"] - $bookPrice) . "  " . $lowBalance . "</strong></font></h6>";
-					} else {
-						print "$" . ($data["balance"] - $bookPrice);
-					}
-					?>
-				</td>
-			</tr>
-		</table>
-	</div>
-	<div>
 
+					
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <h3 align="center">YOUR ACCOUNT BALANCE:</h3>
+                <table class="table table-bordered">
+                    <thead class="thead-light">
+                        <tr>
+                            <th>BALANCE</th>
+                        </tr>
+                    </thead>
+                    <tr>
+                        <td>
+                            <?php
+							if($lowBalance!=null){
+								print "<h6><font color=red><strong>$" . ($data["balance"] - $bookPrice) . "  " . $lowBalance . "</strong></font></h6>";
+							} else {
+								print "$" . ($data["balance"] - $bookPrice);
+							}
+						?>
+                        </td>
+                    </tr>
+                </table>
+            </div>
 
-		<h3>Books you are selling:</h3>
-		<table align="center" border="2" width=400>
-			<tr>
-				<th>
-					Title
-				</th>
-				<th>
-					Post Time
-				</th>
-				<th>
-					Picture
-				</th>
-			</tr>
-			<?php
-			 while($row = mysqli_fetch_assoc($results)) {
+            <div class="col-4">
+                <h3 align="center">BOOKS YOU ARE SELLING:</h3>
+                <table class="table table-bordered">
+                <thead class="thead-light">
+                    <tr>
+                        <th>Title</th>
+                        <th>Post Time</th>
+                        <th>Picture</th>
+                    </tr>
+                    </thead>
+                    <?php
+			while($row = mysqli_fetch_assoc($results)) {
 					print "<tr>";
 					print "<td>";
 					print($row["title"]);
@@ -134,13 +146,45 @@
 					print "</td>";
 					print "<td>";
 					print "<img src ='";
-					print $row["pic_path"] . "' height = 50 width = 50>";
+					print $row["pic_path"] . "' height = 75 width = 55>";
 					print "</td>";
 					print "</tr>";
 				}
 			?>
-		</table>
-	</div>
+                </table>
+            </div>
+
+            <div class="col-3">
+                <?php
+		$bookPrice = 0;
+		$selectBooks = "SELECT * FROM books b JOIN cart c where b.isbn10 = c.isbn10;";
+		$results = mysqli_query($connect, $selectBooks);
+		if($results->num_rows==0){
+			print("<h3>EMPTY CART</h3>");
+		} else{
+			print("IN YOUR CART:<br> <a href=bookBuy.php>Buy</a>");
+			while($row = mysqli_fetch_assoc($results)) 
+			{
+				print "<p>" . ($row["title"]) . "</p>";
+				print "<p>" . ($row["price"]) . "</p>";
+				$bookPrice = $bookPrice + $row["price"];
+				print "<a href='deleteBook.php?isbn10=".($row["isbn10"]) ."'>Delete</a>";
+				print "<p><img src='" . $row["pic_path"] . "' height = 190 width = 150></p>";
+			}
+		}
+		?>
+            </div>
+        </div>
+
+
+    </div>
+    </div>
+
+
+
+
+    </div>
+    </div>
 </body>
 
 </html>
