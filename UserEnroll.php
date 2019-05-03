4b5364689a8e9d53a22071d9e9a7a61723250e45
Kevin J Crespin <kevinjonathan290@gmail.com>
2019-05-03 00:23:22 -0700

    Touched userEnroll
    UserEnroll.css is not needed anymore (Using login.css instead)

--- UserEnroll.php
+++ UserEnroll.php
@@ -1,59 +1,34 @@
 <!DOCTYPE html>
 <html>
-
 <head>
-    <meta charset="UTF-8">
-    <title>User Enrollment</title>
-    <link href="UserEnroll.css" type="text/css" rel="stylesheet" />
-    <style>
-    th {
-        background-color: #4a4f55;
-        color: #ffffff;
-        font-weight: normal;
-    }
-
-    #loginLink {
-        background-color: #8a8a8d;
-        border: 1px solid #8a8a8d;
-        font-size: 14px;
-        text-decoration: none;
-        padding: 2px;
-    }
-
-    </style>
+<meta charset="UTF-8">
+<title>Book Exchange Login Page</title>
+<link href="login.css" type="text/css" rel="stylesheet" />
 </head>
-
 <body>
-    <div align="center">
-        <form action="UserInsert.php" method="post">
-            <img src="images/brand_horizontal_logo_4color.png" alt="Cal State LA" id="pic1" align="center;">
-            <table>
-                <tr>
-                    <td>
-                        <h2 id="title" align="center">USER ENROLL</h2id>
-                    </td>
-                </tr>
-                <tr>
-                    <td align="center">USERNAME: <input type="text" name="name" id="text1" /></td>
-                </tr>
-                <tr>
-                    <td align="center">EMAIL: <input type="text" name="email" id="text2" /></td>
-                </tr>
-                <tr>
-                    <td align="center"><input type="submit" value="REGISTER" id="submit" /></td>
-                </tr>
-
-                <tr>
-                    <td align="center">
-                        <p id="adminLink"><a href="/admin.php">BACK TO ADMIN</a></p>
-                    </td>
-                </tr>
-                <tr>
-                    <td align="center"><a href="login.html" id="loginLink">BACK TO LOGIN</a></td>
-                </tr>
-            </table>
-        </form>
-    </div>
+<div align="center">
+<form action="UserInsert.php" method="post" style="
+    width: 340px; 
+    height: 260px; 
+    background: rgba(255, 254, 254, 0.80);
+    top: 50%;
+    left: 50%;
+    position: absolute;
+    transform: translate(-50%, -50%);
+    border-radius: 5%;">
+<a href="Login.html"><img src="images/brand_horizontal_logo_4color.png" alt="Cal State LA" id="pic1" align="center;"></a>
+<table align="center">
+    <tr>
+        <td align="center">NAME: <input type="text" name="name" id="text1" required/></td>
+    </tr>
+    <tr>
+        <td align="center">EMAIL: <input type="email" name="email" id="text2" required/></td>
+    </tr>
+    <tr>
+        <td align="center" colspan="2"><input type="submit" name="submit" value="REGISTER" id="submit"/></td>
+    </tr>
+</table>
+</form>
+</div>
 </body>
-
 </html>
